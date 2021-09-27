d95bb11a734e9f34a7414823f540996ae6eee972
FrancescoBianchi7 <bianch1francesco@gmail.com>
2021-09-27 12:50:35 +0200

    creazione

---
+++ cmake-build-debug/CMakeFiles/VideoGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoGame.dir/main.cpp.obj"
+  "VideoGame.exe"
+  "VideoGame.exe.manifest"
+  "VideoGame.pdb"
+  "libVideoGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
